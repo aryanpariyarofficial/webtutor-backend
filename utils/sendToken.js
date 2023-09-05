84cb1dfe42300e7c9ddb879b07dc5de8266beb5b
aryanpariyarofficial <aryanpariyar8463@gmail.com>
2023-09-05 23:39:31 +0545

    change

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
     const options = {
       expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      // secure: true, // only comment on local host uncommet in live server
+      secure: true, // only comment on local host uncommet in live server
       
       sameSite: "none",
     };
